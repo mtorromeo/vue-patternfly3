823bd673e0e89382c94cbb8d1ad578dc388b6595
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2017-09-10 21:58:05 +0200

    Set default href for LauncherItems

--- src/components/LauncherItem.vue
+++ src/components/LauncherItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="applauncher-pf-item" role="menuitem">
-    <a class="applauncher-pf-link" v-bind="$attrs" v-on="$listeners">
+    <a class="applauncher-pf-link" href="javascript:void(0)" v-bind="$attrs" v-on="$listeners">
       <i class="applauncher-pf-link-icon pficon" :class="iconClass" aria-hidden="true"></i>
       <span class="applauncher-pf-link-title"><slot></slot></span>
     </a>
