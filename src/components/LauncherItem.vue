64e8e7bebd99e0885f558c1ccb784c0c17721424
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2017-12-21 23:28:49 +0100

    Make pf-launcher-item accept href property for its link

--- src/components/LauncherItem.vue
+++ src/components/LauncherItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="applauncher-pf-item" role="menuitem">
-    <a class="applauncher-pf-link" href="javascript:void(0)" v-bind="$attrs" v-on="$listeners">
+    <a class="applauncher-pf-link" :href="href" v-bind="$attrs" v-on="$listeners">
       <i class="applauncher-pf-link-icon pficon" :class="iconClass" aria-hidden="true"></i>
       <span class="applauncher-pf-link-title"><slot></slot></span>
     </a>
@@ -12,6 +12,10 @@ export default {
   name: 'pf-launcher-item',
 
   props: {
+    href: {
+      type: String,
+      default: 'javascript:void(0)',
+    },
     iconClass: String,
   },
 };
