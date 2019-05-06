f441a3255d8834e40cd40de8bfc50708fbde8619
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2019-05-06 12:32:31 +0200

    Updated dependencies

--- src/components/ModalsTarget.vue
+++ src/components/ModalsTarget.vue
@@ -1,30 +1,44 @@
 <template functional>
-  <portal-target name="modals-target" :transition="{name: 'drop-fade'}" multiple/>
+  <portal-target name="modals-target" transition="pf-drop-fade" multiple/>
 </template>
 
 <style>
-.modal-backdrop.drop-fade-enter-active,
-.modal-backdrop.drop-fade-leave-active,
-.modal.drop-fade-enter-active,
-.modal.drop-fade-leave-active {
+.modal-backdrop.pf-drop-fade-enter-active,
+.modal-backdrop.pf-drop-fade-leave-active,
+.modal.pf-drop-fade-enter-active,
+.modal.pf-drop-fade-leave-active {
   transition: transform .2s linear, opacity .2s linear;
 }
 
-.modal.drop-fade-enter,
-.modal.drop-fade-leave-to {
+.modal.pf-drop-fade-enter,
+.modal.pf-drop-fade-leave-to {
   transform: translateY(-100%);
 }
 
-.modal.drop-fade-enter,
-.modal.drop-fade-leave-to,
-.modal-backdrop.drop-fade-enter,
-.modal-backdrop.drop-fade-leave-to {
+.modal.pf-drop-fade-enter,
+.modal.pf-drop-fade-leave-to,
+.modal-backdrop.pf-drop-fade-enter,
+.modal-backdrop.pf-drop-fade-leave-to {
   opacity: 0;
 }
 </style>
 
 <script>
+import Vue from 'vue';
 import {PortalTarget} from 'portal-vue';
+
+Vue.component('pf-drop-fade', {
+  functional: true,
+  render(createElement, context) {
+    const data = {
+      props: {
+        name: 'pf-drop-fade',
+        mode: 'out-in',
+      },
+    };
+    return createElement('transition-group', data, context.children);
+  },
+});
 
 export default {
   name: 'pf-modal-target',
