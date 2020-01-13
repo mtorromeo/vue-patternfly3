249626f9296989e28bff92e05dbf843df5b586c3
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2020-01-13 18:02:20 +0100

    Reorganized dist output with multiple module formats and splitted code for components

--- rollup.config.js
+++ rollup.config.js
@@ -1,63 +1,158 @@
-/* global __dirname */
-
-import path from 'path';
 import vue from 'rollup-plugin-vue';
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import json from 'rollup-plugin-json';
-import css from 'rollup-plugin-css-only';
+import embedCSS from 'rollup-plugin-embed-css';
+import cssPorter from 'rollup-plugin-css-porter';
 import commonjs from 'rollup-plugin-commonjs';
+import {terser} from 'rollup-plugin-terser';
+import omt from '@surma/rollup-plugin-off-main-thread';
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
 
-const src = path.resolve(__dirname, 'src');
-const dist = path.resolve(__dirname, 'dist');
-const {name} = require('./package.json');
+const readdir = util.promisify(fs.readdir);
 
 const external = ['vue', 'uiv', 'c3'];
-
-module.exports = {
-  input: path.resolve(src, 'index.js'),
-  external,
-  plugins: [
-    // we just want to remove css, webpack already builds a correct stylesheet
-    css({output: false}),
-    vue({
-      css: false,
-      style: {
-        preprocessOptions: {
-          scss: {
-            // node-sass custom importer to resolve tilde prefix to local node_modules dir
-            importer: url => ({
-              file: url.replace(/^~/, 'node_modules/'),
-            }),
-          },
-        },
-      },
-    }),
-    json(),
-    resolve({external}),
-    commonjs({
-      namedExports: {
-        'node_modules/portal-vue/dist/portal-vue.js': ['PortalTarget', 'Portal'],
-      },
-    }),
-    babel({
-      babelrc: false,
-      presets: [
-        ['@babel/preset-env', {modules: false}],
-      ],
-      plugins: [
-        '@babel/plugin-transform-regenerator',
-        '@babel/plugin-transform-async-to-generator',
-        '@babel/plugin-transform-destructuring',
-        '@babel/plugin-proposal-object-rest-spread',
-      ],
-    }),
-  ],
-  output: [
-    {
-      format: 'es',
-      file: path.resolve(dist, `${name}.esm.js`),
-      sourcemap: true,
-    },
-  ],
+const globals = {
+  c3: 'c3',
+  vue: 'Vue',
+  uiv: 'uiv',
 };
+
+const input = {
+  index: 'src',
+};
+
+const baseVueOptions = {
+  style: {
+    preprocessOptions: {
+      scss: {
+        // node-sass custom importer to resolve tilde prefix to local node_modules dir
+        importer: url => ({
+          file: url.replace(/^~/, 'node_modules/'),
+        }),
+      },
+    },
+  },
+};
+
+export default (async () => {
+  const files = await readdir('src/components');
+
+  for (const f of files) {
+    const ext = path.extname(f);
+    const name = path.basename(f, ext);
+
+    if (name === 'index') {
+      continue;
+    }
+
+    const distName = name.replace(/([A-Z])/g, ' $1').trim().replace(/ /g, '-').toLowerCase();
+    input[distName] = `src/components/${f}`;
+  }
+
+  const baseConfig = {
+    input,
+    external,
+    plugins: [
+      json(),
+      resolve({external}),
+      commonjs({
+        namedExports: {
+          'node_modules/portal-vue/dist/portal-vue.js': ['PortalTarget', 'Portal'],
+        },
+      }),
+      babel({
+        babelrc: false,
+        presets: [
+          ['@babel/preset-env', {modules: false}],
+        ],
+        plugins: [
+          '@babel/plugin-transform-regenerator',
+          '@babel/plugin-transform-async-to-generator',
+          '@babel/plugin-transform-destructuring',
+          '@babel/plugin-proposal-object-rest-spread',
+        ],
+      }),
+    ],
+  };
+
+  function config(css = null, vueOptions = {}, extraPlugins = []) {
+    return {
+      ...baseConfig,
+      plugins: [
+        css === null
+        ? embedCSS()
+        : cssPorter(css ? css : {
+          raw: false,
+          minified: false,
+        }),
+        vue({
+          ...baseVueOptions,
+          ...vueOptions,
+        }),
+        ...baseConfig.plugins,
+        ...extraPlugins,
+      ],
+      output: {
+        dir: 'dist/esm',
+        format: 'esm',
+        globals,
+      },
+    };
+  }
+
+  return [
+    {
+      ...config(),
+      output: {
+        dir: 'dist/esm',
+        format: 'esm',
+        globals,
+      },
+    },
+    {
+      ...config(null, {}, [omt(), terser()]),
+      output: {
+        dir: 'dist/amd',
+        format: 'amd',
+        sourcemap: true,
+        globals,
+      },
+    },
+    {
+      ...config(false, {template: {optimizeSSR: true}}),
+      output: {
+        dir: 'dist/cjs',
+        format: 'cjs',
+        globals,
+      },
+    },
+    {
+      ...config(false, {css: false}),
+      input: input.index,
+      output: {
+        file: 'dist/vue-patternfly.js',
+        format: 'iife',
+        sourcemap: true,
+        name: 'VuePatternfly',
+        globals,
+      },
+    },
+    {
+      ...config({
+        raw: 'dist/vue-patternfly.css',
+        minified: 'dist/vue-patternfly.min.css',
+      }, {css: false}, [terser()]),
+      input: input.index,
+      output: {
+        file: 'dist/vue-patternfly.min.js',
+        format: 'iife',
+        sourcemap: true,
+        name: 'VuePatternfly',
+        globals,
+      },
+    },
+  ];
+})();
